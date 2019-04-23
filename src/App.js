805b5d1737df1c89bc974aa5760c3bcc4240759c
a.kennedy <alexander.j.kennedy@leidos.com>
2019-04-24 09:36:55 +1000

    made a dummy log to help me build the hard coded JSON event log

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { ThemeId, currThemeId } from './viewLogic/colourSetManager';
 import { Footer } from './reactComponents/Footer';
 import { DataEventHandlers } from './interactionLayer/dataEventLogger';
 import { RegisterForDataEvents } from './interactionLayer/viewLayerInteractionApi';
+import { LogDummyDataEventObjects } from './interactionLayer/dummy/dummyEventlogDataModel';
 
 class App extends Component {
   constructor(props) {
@@ -16,6 +17,9 @@ class App extends Component {
 
     // Register the logger to receive data event updates sent from the view-layer triggers.
     RegisterForDataEvents(DataEventHandlers);
+
+    // For development purposes, log change events
+    LogDummyDataEventObjects();
 
     // Create a temporary state context for creation forms
     this.formStateManager = TemporaryStateManager();
--- src/interactionLayer/dummy/dummyEventlogDataModel.js
+++ src/interactionLayer/dummy/dummyEventlogDataModel.js
@@ -2,50 +2,63 @@
 // will receive from the server-side once that it actually implemented.
 import { Category, TaskObjects } from '../../logicLayer/Task';
 import { ColourIdTracker } from '../../viewLogic/colourSetManager';
-import {  }
+import { DataEventHandlers } from '../dataEventLogger';
 
-
-export function GetActiveTaskObject() {
+export function LogDummyDataEventObjects() {
     // Create a new task list, and populate with some basic shit.
     let tasklist = new TaskObjects();
 
-    // Create a couple of 'goal' tasks, and a weekly subtask for the second one.
+    console.log("****************************** BEGIN LOG *******************************");
+    
     let goaltask = tasklist.CreateNewIndependentTask('goal task numero uno', Category.Goal, Date.now(), ColourIdTracker.useNextColour());
+    DataEventHandlers.taskAddedHandler(goaltask, tasklist);
+
     let goaltask2 = tasklist.CreateNewIndependentTask('different goal with child', Category.Goal, Date.now(), ColourIdTracker.useNextColour());
+    DataEventHandlers.taskAddedHandler(goaltask2, tasklist);
+
     let subtask = tasklist.CreateNewSubtask('weekly subtask', goaltask2, Date.now());
-    tasklist.CreateNewSubtask('daily sub subby boi', subtask, Date.now());
-    tasklist.CreateNewDailySubtask('This is a skipped subtask', goaltask, Date.now());
-    //let subtask2 = tasklist.CreateNewSubtask('MEMEMEMEMEMES', goaltask2);
-    //tasklist.CreateNewSubtask('A B C D E F G H I J K L M N O P', subtask2);
+    DataEventHandlers.childTaskAddedHandler(goaltask2, subtask, tasklist);
 
-    // Create a weekly task with one daily subtask.
+    let subtask2 = tasklist.CreateNewSubtask('daily sub subby boi', subtask, Date.now());
+    DataEventHandlers.childTaskAddedHandler(subtask, subtask2, tasklist);
+
+    let dailysubtask = tasklist.CreateNewDailySubtask('This is a skipped subtask', goaltask, Date.now());
+    DataEventHandlers.childTaskAddedHandler(goaltask, dailysubtask, tasklist);
+
     let weektask2 = tasklist.CreateNewIndependentTask('independent weekly task', Category.Weekly, Date.now(), ColourIdTracker.useNextColour());
-    tasklist.CreateNewSubtask('daily subtask', weektask2, Date.now());
+    DataEventHandlers.taskAddedHandler(weektask2, tasklist);
 
-    // Create an independent daily task.
-    tasklist.CreateNewIndependentTask('solo daily boy', Category.Daily, Date.now(), ColourIdTracker.useNextColour());
+    let dailysubtask2 = tasklist.CreateNewSubtask('daily subtask', weektask2, Date.now());
+    DataEventHandlers.childTaskAddedHandler(weektask2, dailysubtask2, tasklist);
 
-    // Create some deferred tasks.
-    tasklist.CreateNewIndependentTask('eeeh later', Category.Deferred, Date.now(), ColourIdTracker.useNextColour());
-    tasklist.CreateNewIndependentTask('ill do it soon, for real this time', Category.Deferred, Date.now(), ColourIdTracker.useNextColour());
+    let task = tasklist.CreateNewIndependentTask('solo daily boy', Category.Daily, Date.now(), ColourIdTracker.useNextColour());
+    DataEventHandlers.taskAddedHandler(task, tasklist);
 
-    // Create some tasks and complete them.
-    tasklist.CompleteTask(tasklist.CreateNewIndependentTask('oompa loompa', Category.Daily, Date.now(), ColourIdTracker.useNextColour()), Date.now());
-    tasklist.CompleteTask(tasklist.CreateNewSubtask('this is a separate subboi!! :)', subtask, Date.now()), Date.now());
+    let task2 = tasklist.CreateNewIndependentTask('eeeh later', Category.Deferred, Date.now(), ColourIdTracker.useNextColour());
+    DataEventHandlers.taskAddedHandler(task2, tasklist);
+    
+    let task3 = tasklist.CreateNewIndependentTask('ill do it soon, for real this time', Category.Deferred, Date.now(), ColourIdTracker.useNextColour());
+    DataEventHandlers.taskAddedHandler(task3, tasklist);
 
-    // Create some tasks and fail them.
-    tasklist.FailTask(tasklist.CreateNewIndependentTask('memes', Category.Daily, Date.now(), ColourIdTracker.useNextColour()), Date.now());
+    let task4 = tasklist.CreateNewIndependentTask('oompa loompa', Category.Daily, Date.now(), ColourIdTracker.useNextColour());
+    DataEventHandlers.taskAddedHandler(task4, tasklist);
 
-    // TEST LOAD: make 20,000 tasks, failing and completing every second one.
-    for (let i=0; i < 0; i++) {
-        let task = tasklist.CreateNewIndependentTask(i, Category.Daily, Date.now(), ColourIdTracker.useNextColour());
-        if (i % 2 === 0) {
-            tasklist.CompleteTask(task, Date.now());
-        }
-        else {
-            tasklist.FailTask(task, Date.now());
-        }
-    }
+    tasklist.CompleteTask(task4, Date.now());
+    DataEventHandlers.taskCompletedHandler(task4, tasklist);
+    
+    let task5 = tasklist.CreateNewSubtask('this is a separate subboi!! :)', subtask, Date.now());
+    DataEventHandlers.childTaskAddedHandler(subtask, task5, tasklist);
+
+    tasklist.CompleteTask(task5, Date.now());
+    DataEventHandlers.taskCompletedHandler(task5, tasklist);
+
+    let task6 = tasklist.CreateNewIndependentTask('memes', Category.Daily, Date.now(), ColourIdTracker.useNextColour());
+    DataEventHandlers.taskAddedHandler(task6, tasklist);
+
+    tasklist.FailTask(task6, Date.now());
+    DataEventHandlers.taskFailedHandler(task6, tasklist);
+
+    console.log("****************************** END LOG *******************************");
 
     return tasklist;
 }
