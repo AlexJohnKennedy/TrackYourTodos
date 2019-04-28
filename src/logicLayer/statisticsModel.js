4f880f20296d1ade1576c020563ce5a8bb4a3f35
AlexJohnKennedy <alexkfridges@gmail.com>
2019-04-28 21:59:50 +1000

    generalised the stat calculation logic

--- src/logicLayer/statisticsModel.js
+++ src/logicLayer/statisticsModel.js
@@ -47,7 +47,115 @@ export class StatisticsModel {
         this.allFailed.push(task);
     }
 
-    GetStatistics() {
+    // OPTIONS:
+    // {
+    //      days:   <-- Number of days to go back. 0 means do not calculate daily stats
+    //      weeks:  <-- Number of weeks to go back. 0 means do not calculate weekly stats
+    //      months: <-- Number of months to go back. 0 means do not calculate monthly stats
+    //      years:  <-- Number of years to go back. 0 means do not calculate yearly stats
+    //      alltime: bool <-- Whether or not to calculate all time stats   
+    // }
+    GetStatistics(optionsObj) {
+        const cmpFunc = (date, groupElem) => {
+            if (date.valueOf() > groupElem.time.valueOf()) {
+                return -1;  // Go before (in the array) if our time is more recent, i.e., value is larger
+            }
+            else if (date.valueOf() < groupElem.time.valueOf()) {
+                return 1;   // Go after (in the array) if our time is less recent, i.e., value is smaller
+            }
+            else {
+                return 0;
+            }
+        };
+        
+        let dayStats = this.calcStats(optionsObj.days, this.dayGroupsCompleted, this.dayGroupsFailed, cmpFunc, date => date.setHours(0, 0, 0, 0), date => date.setDate(date.getDate() - 1));
+        let weekStats = this.calcStats(optionsObj.weeks, this.weekGroupsCompleted, this.weekGroupsFailed, cmpFunc, date => {
+            date.setDate(date.getDate() - date.getDay() + 1);
+            date.setHours(0, 0, 0, 0);
+            return date;
+        }, date => date.setDate(date.getDate() - 2));
+        let monthStats = this.calcStats(optionsObj.months, this.monthGroupsCompleted, this.monthGroupsFailed, cmpFunc, date => new Date(date.getFullYear(), date.getMonth()), date => date.setDate(date.getDate() - 1));
+        let yearStats = this.calcYearStats(optionsObj.years, this.yearGroupsCompleted, this.yearGroupsFailed, cmpFunc, date => new Date(date.getFullYear(), 0), date => date.setDate(date.getDate() - 5));
+        let alltimeStats = optionsObj.alltime ? this.calcAlltimeStats() : null;
 
+        return {
+            dayStats: dayStats,
+            weekStats: weekStats,
+            monthStats: monthStats,
+            yearStats: yearStats,
+            alltimeStats: alltimeStats
+        };
     }
+    
+    calcStats(numToGoBack, completedGroups, failedGroups, cmpFunc, floorDateToKeyDateFunc, cycleBackFunc) {
+        if (numToGoBack <= 0) return null;
+        
+        let completedArray = [];
+        let failedArray    = [];
+        let completedAggregate = 0;
+        let failedAggregate    = 0;
+
+        let currDate = floorDateToKeyDateFunc(new Date(Date.now()));
+
+        for (let i=0; i < numToGoBack; i++) {
+            let completedSearchResult = binarySearch(completedGroups, currDate, cmpFunc);
+            if (completedSearchResult < 0) {
+                completedArray.push(0);  // No completed tasks for this day
+            }
+            else {
+                completedArray.push(completedGroups[completedSearchResult].tasks.length);
+                completedAggregate += completedGroups[completedSearchResult].tasks.length;
+            }
+
+            let failedSearchResult = binarySearch(failedGroups, currDate, cmpFunc);
+            if (failedSearchResult < 0) {
+                failedArray.push(0);  // No completed tasks for this day
+            }
+            else {
+                failedArray.push(failedGroups[failedSearchResult].tasks.length);
+                failedAggregate += failedGroups[failedSearchResult].tasks.length;
+            }
+
+            // Cycle back one time-step, and look again
+            currDate = cycleBackFunc(currDate);
+            currDate = floorDateToKeyDateFunc(currDate);
+        }
+
+        return {
+            numCompletedArray: completedArray,
+            numFailedArray: failedArray,
+            totalCompleted: completedAggregate,
+            totalFailed: failedAggregate
+        };
+    }
+}
+
+/*
+ * Binary search in JavaScript.
+ * Returns the index of of the element in a sorted array or (-n-1) where n is the insertion point for the new element.
+ * Parameters:
+ *     ar - A sorted array
+ *     el - An element to search for
+ *     compare_fn - A comparator function. The function takes two arguments: (a, b) and returns:
+ *        a negative number  if a is less than b;
+ *        0 if a is equal to b;
+ *        a positive number of a is greater than b.
+ * The array may contain duplicate elements. If there are more than one equal elements in the array, 
+ * the returned value can be the index of any one of the equal elements.
+ */
+function binarySearch(ar, el, compare_fn) {
+    var m = 0;
+    var n = ar.length - 1;
+    while (m <= n) {
+        var k = (n + m) >> 1;
+        var cmp = compare_fn(el, ar[k]);
+        if (cmp > 0) {
+            m = k + 1;
+        } else if(cmp < 0) {
+            n = k - 1;
+        } else {
+            return k;
+        }
+    }
+    return -m - 1;
 }
