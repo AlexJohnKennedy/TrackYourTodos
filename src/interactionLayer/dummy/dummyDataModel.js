6d880fc7915c8805c395e68bfbb9ee3ba6b89f7e
AlexJohnKennedy <alexkfridges@gmail.com>
2019-04-28 10:59:59 +1000

    removed debug statements

--- src/interactionLayer/dummy/dummyDataModel.js
+++ src/interactionLayer/dummy/dummyDataModel.js
@@ -2,7 +2,6 @@
 import { Category, TaskObjects, SetIdStartVal } from '../../logicLayer/Task';
 import { ColourIdTracker } from '../../viewLogic/colourSetManager';
 import { RebuildState } from '../../logicLayer/StateRebuilder';
-import { RegisterForFailureChecking } from '../../logicLayer/checkForFailure';
 
 // Dummy data log
 import { dummyEventLog, dummyEventLog2 } from './dummyEventlogDataModel';
@@ -11,10 +10,6 @@ export function GetActiveTaskObject() {
     let tasklist = new TaskObjects();
     SetIdStartVal(RebuildState(dummyEventLog, tasklist) + 1);
     SetIdStartVal(RebuildState(dummyEventLog2, tasklist) + 1);
-
-    // DEBUG:
-    let failureChecker = RegisterForFailureChecking(tasklist);
-    console.log(failureChecker.PeekTasksToFail());
 
     return tasklist;
 }
