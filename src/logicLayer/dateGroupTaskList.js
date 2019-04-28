b98fde1848fa6bcc8321fb902962981f21dbb30f
AlexJohnKennedy <alexkfridges@gmail.com>
2019-04-28 15:54:21 +1000

    started implementing a date grouped system in the logic layer, to facilitate more efficient statistics generation

---
+++ src/logicLayer/dateGroupTaskList.js
@@ -0,0 +1,118 @@
+// This exports a data structure used to group tasks into a 'time bracket' (e.g. month), and then store all
+// tasks into an array based on that grouping, such that a list of all tasks in time-sorted order can be
+// restored.
+
+// Represents the possible grouping types.
+export const TimeGroupTypes = Object.freeze({
+    DAY: 0,
+    WEEK: 1,
+    MONTH: 2,
+    YEAR: 3
+});
+
+export function BuildNewTimeGroupedTaskList(groupingType, sortByActivationTime = false) {
+    // Sort tasks by either activation time, or time closed, depending on parameter.
+    let timeKeyFunc = sortByActivationTime ? t => t.eventTimestamps.timeActivated : t => t.eventTimestamps.timeClosed;
+
+    if (groupingType === TimeGroupTypes.DAY) {
+        throw new Error("Sorry, daily time grouping isn't implemented yet! I'm bad :(");
+    }
+    else if (groupingType === TimeGroupTypes.MONTH) {
+        return new MonthGroupedTaskList(timeKeyFunc);
+    }
+    else if (groupingType === TimeGroupTypes.YEAR) {
+        throw new Error("Sorry, yearly time grouping isn't implemented yet! I'm bad :(");
+    }
+    else {
+        throw new Error("Illegal grouping type passed to BuildNewTimeGroupedTaskList: " + groupingType);
+    }
+}
+
+class MonthGroupedTaskList {
+    constructor(taskTimeKeyFunction) {
+        this.groups = [];
+        this.taskTimeKeyFunction = taskTimeKeyFunction;
+    }
+
+    AddTask(task) {
+        let timeStamp = this.taskTimeKeyFunction(task);
+        if (timeStamp === null || timeStamp === undefined) { throw new Error('error is time key func. Returned undefined!'); }
+
+        // Create a date which represents the exact starting moment (local time) of the month of this task's timestamp!
+        let timestampDate = new Date(timeStamp);
+        let monthDate = new Date(timestampDate.getFullYear(), timestampDate.getMonth());
+
+        // DEBUG:
+        console.log("task time stamp:");
+        console.log(timestampDate);
+        console.log("task timestamp month date: ");
+        console.log(monthDate);
+
+        // Okay. We know the groups array is sorted by months, so just cycle that list until we find the correct 'insertion point'.
+        let searchResult = binarySearch(this.groups, monthDate, (monthDate, groupElem) => {
+            if (monthDate.valueOf() > groupElem.time.valueOf()) {
+                return -1;  // Go before (in the array) if our time is more recent, i.e., value is larger
+            }
+            else if (monthDate.valueOf() < groupElem.time.valueOf()) {
+                return 1;   // Go after (in the array) if our time is less recent, i.e., value is smaller
+            }
+            else {
+                return 0;
+            }
+        })
+        if (searchResult < 0) {
+            // No match was found, thus, we need to add a new 'group' at position (-searchResult - 1)
+            this.groups.splice((-searchResult-1), 0, {
+                time: monthDate,
+                tasks: [task]
+            });
+        }
+        else {
+            // Need to add this task into the matched group.
+            let taskList = this.groups[searchResult].tasks;
+            let innerSearchResult = binarySearch(taskList, task, (a, b) => {
+                return this.taskTimeKeyFunction(b) - this.taskTimeKeyFunction(a);
+            });
+            if (innerSearchResult < 0) {
+                this.groups[searchResult].tasks.splice((-innerSearchResult-1), 0, task);
+            }
+            else {
+                this.groups[searchResult].tasks.splice(innerSearchResult, 0, task);
+            }
+        }
+    }
+
+    GetAllGroupedTasks() {
+        return this.groups.slice(0);    // Shallow copy.
+    }
+}
+
+/*
+ * Binary search in JavaScript.
+ * Returns the index of of the element in a sorted array or (-n-1) where n is the insertion point for the new element.
+ * Parameters:
+ *     ar - A sorted array
+ *     el - An element to search for
+ *     compare_fn - A comparator function. The function takes two arguments: (a, b) and returns:
+ *        a negative number  if a is less than b;
+ *        0 if a is equal to b;
+ *        a positive number of a is greater than b.
+ * The array may contain duplicate elements. If there are more than one equal elements in the array, 
+ * the returned value can be the index of any one of the equal elements.
+ */
+function binarySearch(ar, el, compare_fn) {
+    var m = 0;
+    var n = ar.length - 1;
+    while (m <= n) {
+        var k = (n + m) >> 1;
+        var cmp = compare_fn(el, ar[k]);
+        if (cmp > 0) {
+            m = k + 1;
+        } else if(cmp < 0) {
+            n = k - 1;
+        } else {
+            return k;
+        }
+    }
+    return -m - 1;
+}
