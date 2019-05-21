9ccdacdcbccedeee1d500436c5a13a756631859a
Alex Kennedy <KennedyAlexJohn@gmail.com>
2019-05-21 16:40:37 +1000

    re-implemented the Database context becuase i fucking forgot to start tracking it on my other computer :(

---
+++ DataTransferLayer/DatabaseContext.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using todo_app.DataTransferLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace todo_app.DataTransferLayer.DatabaseContext {
+    public class TodoEventContext : DbContext {
+        public TodoEventContext(DbContextOptions<TodoEventContext> options) : base(options) {
+            /* Do nothing */
+        }
+
+        public DbSet<GenericTodoEvent> TodoEvents { get; set; }
+
+        // Provide a value converter for the children array, since arrays of primitives cannot be mapped to a database.
+        // We will convert this array into a comma separated string, so it does not require storage in an additional table.
+        // SEE: "Value Conversions" in Entity Framework Core documentation
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<GenericTodoEvent>().Property(e => e.Children).HasConversion(
+                intArray => string.Join(",", intArray),
+                commaSeparatedString => commaSeparatedString.Split(",", StringSplitOptions.RemoveEmptyEntries).Select(s => int.Parse(s)).ToArray()
+            );
+        }
+    }
+}
