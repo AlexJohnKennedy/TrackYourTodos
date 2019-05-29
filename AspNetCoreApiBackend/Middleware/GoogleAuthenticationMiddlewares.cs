d5caa3bee996d57c3d12aa1e98f1c84227cf8899
Alex Kennedy <KennedyAlexJohn@gmail.com>
2019-05-29 22:26:57 +1000

    managed to setup token settings from a callback which allows us to access the HttpContext from inside the JwtBearerHandler control-flow. This means I should be able to insert either my custom-pkey-fetching data in here at the request-scope level, OR simply invoke the Google dotnet client library to do the token validation for us entirely.

---
+++ AspNetCoreApiBackend/Middleware/GoogleAuthenticationMiddlewares.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace todo_app.GoogleAuthenticationMiddlewares {
+
+}
--- AspNetCoreApiBackend/Startup.cs
+++ AspNetCoreApiBackend/Startup.cs
@@ -1,9 +1,11 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -61,32 +63,70 @@ namespace todo_app
                 authOptions.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
                 authOptions.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
             }).AddJwtBearer(jwtOptions => {
-                
+                //jwtOptions.Audience = "918054703402-2u53f3l62mpekrao3jkqd6geg3mjvtjq.apps.googleusercontent.com";
+                //jwtOptions.Authority = "accounts.google.com";
+                //jwtOptions.RefreshOnIssuerKeyNotFound = true;
+                //jwtOptions.Challenge = "Bearer";
                 //jwtOptions.MetadataAddress = "https://www.googleapis.com/oauth2/v3/certs";
+                //jwtOptions.BackchannelHttpHandler = new HttpClientHandler();
+
+                // The JWT handler will call this event with the data i need to plug in either the GoogleClient library validator, OR, just read 
+                // data from the HttpContext which has had the manually-fetched/cached Google Public Key attached to it! :O 
+                jwtOptions.Events = new JwtBearerEvents() {
+                    OnMessageReceived = async (messageContext) => {
+                        // From here, we can access the context surrounding the current request!
+                        // We should be able to use this to read the public-keys attached to HttpContext.Items from the previous middleware,
+                        // which will be custom middleware to fetch the keys (if need be) from https://www.googleapis.com/oauth2/v3/certs.
+                        // we can then insert that key into the TokenValidationParameters here, and voila! (... hopefully)
+                        JwtBearerOptions optionsObj = messageContext.Options;
+                        HttpContext httpContext = messageContext.HttpContext;
+                        optionsObj.TokenValidationParameters = new TokenValidationParameters {
+                            ValidateAudience = true,
+                            ValidAudience = "918054703402-2u53f3l62mpekrao3jkqd6geg3mjvtjq.apps.googleusercontent.com",
+                            ValidateIssuer = true,
+                            ValidIssuer = "accounts.google.com",
+                            ValidateLifetime = true,
+                            IssuerSigningKeyResolver = (tokenString, securityTokenObj, kidString, validationParamsObj) => {
+
+                                string jwkResult = "{\"keys\":[{\"kid\":\"07a082839f2e71a9bf6c596996b94739785afdc3\",\"e\":\"AQAB\",\"kty\":\"RSA\",\"alg\":\"RS256\",\"n\":\"9Y5kfSJyw-GyM4lSXNCVaMKmDdOkYdu5ZhQ7E-8nfae-CPPsx3IZjdUrrv_AoKhM3vsZW_Z3Vucou53YZQuHFpnAa6YxiG9ntpScviU1dhMd4YyUtNYWVBxgNemT9dhhj2i32ez0tOj7o0tGh2Yoo2LiSXRDT-m2zwBImYkBksws4qq_X3jZhlfYkznrCJGjVhKEHzlQy5BBqtQtN5dXFVi-zRZ0-m7oiNW_2wivjw_99li087PNFSeyHpgxjbg30K2qnm1T8gVhnzqf8xnPW9vZFyc_8-3qmbQeDedB8YWyzojM3hDLsHqypP84MSOmejmi0c2b836oc-pI8seXwQ\",\"use\":\"sig\"},{\"e\":\"AQAB\",\"kty\":\"RSA\",\"alg\":\"RS256\",\"n\":\"uNgHfkBGmFZbxa7E0tnsRzarMiE26hnGfwf6PPIbMClW_-VqwwTPXftsnrPbQg93XAKS-r1jQZL54vpVbKJjT7V0TwGpEHplIlLQdSiHLvcUTKPi8ZdG2Promst8khxHTbNDvBhtJwsYXUwLJ3fDF-6v2kTZCHnggQF-tPo4Jumb0WoZYXN74VxmcBU52ypEAlGCxECwpVYOQlrJUzRl7BXvP1EI24D_ZLPLLZd1oP0zz4bFTXeHYm1Q79y8UVBH6o-7nrw9MC1150lSCXX-tXnVJ53f1U2V8PhyDNVy9feTBMO06mvyhl5b3E0aHptgTBUeSFcCIGMvjMgVAO2brw\",\"use\":\"sig\",\"kid\":\"c7f522d032284d252bee4fd80560cefa0fb60c39\"}]}";
+                                JObject rootJsonObj = JObject.Parse(jwkResult);
+                                JArray keysArray = JArray.Parse(rootJsonObj["keys"].ToString());
+                                List<SecurityKey> l = new List<SecurityKey>();
+                                foreach (JToken item in keysArray) {
+                                    l.Add(new JsonWebKey(item.ToString()));
+                                }
+                                return l;
+                            }
+                        };
+
+                        // Alternatively, we can directly override everything using the Google Client Library package.. we'll see!
+                    }
+                };
 
                 // Specify the token validation paramters; I.e., what steps should be taken in order to fully verify the validity of an incoming token.
                 // (e.g., check it is signed by google, by using Google's public key, check expiry time, etc.)
-                jwtOptions.TokenValidationParameters = new TokenValidationParameters {
-                    ValidateAudience = true,
-                    ValidAudience = "918054703402-2u53f3l62mpekrao3jkqd6geg3mjvtjq.apps.googleusercontent.com",
-                
-                    ValidateIssuer = true,
-                    ValidIssuer = "accounts.google.com",
-                
-                    ValidateLifetime = true,
-                    
-                    /* Assign a delegate (of type 'IssuerSigningKeyResolver') which fetches/returns google's current public key! */
-                    IssuerSigningKeyResolver = (tokenString, securityTokenObj, kidString, validationParamsObj) => {
-                        string jwkResult = "{\"keys\":[{\"kid\":\"07a082839f2e71a9bf6c596996b94739785afdc3\",\"e\":\"AQAB\",\"kty\":\"RSA\",\"alg\":\"RS256\",\"n\":\"9Y5kfSJyw-GyM4lSXNCVaMKmDdOkYdu5ZhQ7E-8nfae-CPPsx3IZjdUrrv_AoKhM3vsZW_Z3Vucou53YZQuHFpnAa6YxiG9ntpScviU1dhMd4YyUtNYWVBxgNemT9dhhj2i32ez0tOj7o0tGh2Yoo2LiSXRDT-m2zwBImYkBksws4qq_X3jZhlfYkznrCJGjVhKEHzlQy5BBqtQtN5dXFVi-zRZ0-m7oiNW_2wivjw_99li087PNFSeyHpgxjbg30K2qnm1T8gVhnzqf8xnPW9vZFyc_8-3qmbQeDedB8YWyzojM3hDLsHqypP84MSOmejmi0c2b836oc-pI8seXwQ\",\"use\":\"sig\"},{\"e\":\"AQAB\",\"kty\":\"RSA\",\"alg\":\"RS256\",\"n\":\"uNgHfkBGmFZbxa7E0tnsRzarMiE26hnGfwf6PPIbMClW_-VqwwTPXftsnrPbQg93XAKS-r1jQZL54vpVbKJjT7V0TwGpEHplIlLQdSiHLvcUTKPi8ZdG2Promst8khxHTbNDvBhtJwsYXUwLJ3fDF-6v2kTZCHnggQF-tPo4Jumb0WoZYXN74VxmcBU52ypEAlGCxECwpVYOQlrJUzRl7BXvP1EI24D_ZLPLLZd1oP0zz4bFTXeHYm1Q79y8UVBH6o-7nrw9MC1150lSCXX-tXnVJ53f1U2V8PhyDNVy9feTBMO06mvyhl5b3E0aHptgTBUeSFcCIGMvjMgVAO2brw\",\"use\":\"sig\",\"kid\":\"c7f522d032284d252bee4fd80560cefa0fb60c39\"}]}";
-                        JObject rootJsonObj = JObject.Parse(jwkResult);
-                        JArray keysArray = JArray.Parse(rootJsonObj["keys"].ToString());
-                        List<SecurityKey> l = new List<SecurityKey>();
-                        foreach (JToken item in keysArray) {
-                            l.Add(new JsonWebKey(item.ToString()));
-                        }
-                        return l;
-                    }
-                };
+                //jwtOptions.TokenValidationParameters = new TokenValidationParameters {
+                //    ValidateAudience = true,
+                //    ValidAudience = "918054703402-2u53f3l62mpekrao3jkqd6geg3mjvtjq.apps.googleusercontent.com",
+                //
+                //    ValidateIssuer = true,
+                //    ValidIssuer = "accounts.google.com",
+                //
+                //    ValidateLifetime = true,
+                //
+                //    /* Assign a delegate (of type 'IssuerSigningKeyResolver') which fetches/returns google's current public key! */
+                //    IssuerSigningKeyResolver = (tokenString, securityTokenObj, kidString, validationParamsObj) => {
+                //        
+                //        string jwkResult = "{\"keys\":[{\"kid\":\"07a082839f2e71a9bf6c596996b94739785afdc3\",\"e\":\"AQAB\",\"kty\":\"RSA\",\"alg\":\"RS256\",\"n\":\"9Y5kfSJyw-GyM4lSXNCVaMKmDdOkYdu5ZhQ7E-8nfae-CPPsx3IZjdUrrv_AoKhM3vsZW_Z3Vucou53YZQuHFpnAa6YxiG9ntpScviU1dhMd4YyUtNYWVBxgNemT9dhhj2i32ez0tOj7o0tGh2Yoo2LiSXRDT-m2zwBImYkBksws4qq_X3jZhlfYkznrCJGjVhKEHzlQy5BBqtQtN5dXFVi-zRZ0-m7oiNW_2wivjw_99li087PNFSeyHpgxjbg30K2qnm1T8gVhnzqf8xnPW9vZFyc_8-3qmbQeDedB8YWyzojM3hDLsHqypP84MSOmejmi0c2b836oc-pI8seXwQ\",\"use\":\"sig\"},{\"e\":\"AQAB\",\"kty\":\"RSA\",\"alg\":\"RS256\",\"n\":\"uNgHfkBGmFZbxa7E0tnsRzarMiE26hnGfwf6PPIbMClW_-VqwwTPXftsnrPbQg93XAKS-r1jQZL54vpVbKJjT7V0TwGpEHplIlLQdSiHLvcUTKPi8ZdG2Promst8khxHTbNDvBhtJwsYXUwLJ3fDF-6v2kTZCHnggQF-tPo4Jumb0WoZYXN74VxmcBU52ypEAlGCxECwpVYOQlrJUzRl7BXvP1EI24D_ZLPLLZd1oP0zz4bFTXeHYm1Q79y8UVBH6o-7nrw9MC1150lSCXX-tXnVJ53f1U2V8PhyDNVy9feTBMO06mvyhl5b3E0aHptgTBUeSFcCIGMvjMgVAO2brw\",\"use\":\"sig\",\"kid\":\"c7f522d032284d252bee4fd80560cefa0fb60c39\"}]}";
+                //        JObject rootJsonObj = JObject.Parse(jwkResult);
+                //        JArray keysArray = JArray.Parse(rootJsonObj["keys"].ToString());
+                //        List<SecurityKey> l = new List<SecurityKey>();
+                //        foreach (JToken item in keysArray) {
+                //            l.Add(new JsonWebKey(item.ToString()));
+                //        }
+                //        return l;
+                //    }
+                //};
             });
 
             // Register our EFCore database context with DI, and configure it to be backed by an in-memory database provider.
