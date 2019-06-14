17bda59eea7d849f341b7ac693b9c9e8c489fb20
AlexJohnKennedy <alexkfridges@gmail.com>
2019-06-14 13:22:15 +1000

    domain layer and interaction api implemented. Data event callback array for this edited text event type also added

--- ClientApp/src/interactionLayer/viewLayerInteractionApi.js
+++ ClientApp/src/interactionLayer/viewLayerInteractionApi.js
@@ -77,7 +77,8 @@ export function InstantiateNewDataModelScope(currContext) {
         taskCompletedHandlers : [],
         taskFailedHandlers : [],
         taskActivatedHandlers : [],
-        taskStartedHandlers : []
+        taskStartedHandlers : [],
+        taskEditedHandlers : []
     };
     const DataLoadedFromServerCallbacks = [];
     const DataRefreshedFromServerCallbacks = [];
@@ -136,6 +137,7 @@ export function InstantiateNewDataModelScope(currContext) {
         DataEventCallbackHandlers.taskFailedHandlers.push(dataEventhandlers.taskFailedHandler);
         DataEventCallbackHandlers.taskActivatedHandlers.push(dataEventhandlers.taskActivatedHandler);
         DataEventCallbackHandlers.taskStartedHandlers.push(dataEventhandlers.taskStartedHandler);
+        DataEventCallbackHandlers.taskEditedHandlers.push(dataEventhandlers.taskEditedHandler);
     }
 
     // Exported inner function: Allows clients to register for callbacks when an INITIAL state-build from server data is completed.
@@ -342,6 +344,12 @@ function BuildNewTaskView(domainTaskObj, activeList, viewLayerCallbackList, data
         dataEventCallbacksLists.taskStartedHandlers.forEach(callback => callback(domainTaskObj, activeList));
     }
 
+    function editTaskName(newText) {
+        activeList.EditTaskText(domainTaskObj, newText, Date.now());
+        viewLayerCallbackList.forEach(callback => callback());
+        dataEventCallbacksLists.taskEditedHandlers.forEach(callback => callback(domainTaskObj, activeList));
+    }
+
     // Return the interface object. Immutable!
     return Object.freeze({
         // State properties
@@ -359,7 +367,8 @@ function BuildNewTaskView(domainTaskObj, activeList, viewLayerCallbackList, data
         DeleteTask : deleteTask,
         ActivateTask : activateTask,
         CompleteTask : completeTask,
-        StartTask : startTask
+        StartTask : startTask,
+        EditeTaskName : editTaskName
     });
 }
 
