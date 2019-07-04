644bfd1d269bb2e061252e494053ae02fb1f1592
Alex Kennedy <KennedyAlexJohn@gmail.com>
2019-07-04 18:22:36 +1000

    attempted some firefox styling

--- ClientApp/src/interactionLayer/ajaxDataModules/apiEndpointConfiguration.js
+++ ClientApp/src/interactionLayer/ajaxDataModules/apiEndpointConfiguration.js
@@ -1,4 +1,5 @@
 // Simple definition of which endpoints to hit! These should be configured with environment variables,
 // but i'm lazy so i'm just doing it here for ease and quickness coz I forget how to set environment vars with 
 // create-react-app.
+
 export const API_ENDPOINT = (process.env.NODE_ENV === "development") ? "https://localhost:5001/todoevents" : "https://track-your-todos-api.azurewebsites.net/todoevents";
