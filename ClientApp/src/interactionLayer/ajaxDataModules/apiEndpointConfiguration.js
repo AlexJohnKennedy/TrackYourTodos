58440ed6daf729a4b017e94f688436ee28bb4355
Alex Kennedy <KennedyAlexJohn@gmail.com>
2019-08-15 19:13:36 +1000

    added environment variable to react app, which the docker image sets, which directs the api endpoint to NOT hit localhost or the azure endpoint

--- ClientApp/src/interactionLayer/ajaxDataModules/apiEndpointConfiguration.js
+++ ClientApp/src/interactionLayer/ajaxDataModules/apiEndpointConfiguration.js
@@ -1,7 +1,9 @@
-// Simple definition of which endpoints to hit! These should be configured with environment variables,
-// but i'm lazy so i'm just doing it here for ease and quickness coz I forget how to set environment vars with 
-// create-react-app.
+// Simple definition of which endpoints to hit.
 
-export const API_ADDRESS = (process.env.NODE_ENV === "development") ? "https://localhost:5001" : "https://track-your-todos-api.azurewebsites.net";
+// If the 'API_ADDRESS' environment variable is set, use that one. Else, use the defaults depending on runtime environment.
+// Note, to define react env variables, we must prefix them with "REACT_APP", see: https://create-react-app.dev/docs/adding-custom-environment-variables
+export const API_ADDRESS = (process.env.REACT_APP_API_ADDRESS !== undefined) ? process.env.REACT_APP_API_ADDRESS :
+(process.env.NODE_ENV === "development") ? "https://localhost:5001" : "https://track-your-todos-api.azurewebsites.net";
+
 export const API_ENDPOINT = API_ADDRESS + "/todoevents";
 export const CONTEXT_STATE_ENDPOINT = API_ADDRESS + "/contexts";
